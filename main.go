d7581066e3ad0786af7879a68fc53233c935ccaf
afoim <acofork@qq.com>
2024-10-22 15:56:30 +0800

    添加文章

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ const indexTemplate = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>我的博客</title>
+    <title>二叉树树的博客</title>
     <style>
         body {
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -52,7 +52,7 @@ const indexTemplate = `
     </style>
 </head>
 <body>
-    <h1>我的博客</h1>
+    <h1>二叉树树的博客</h1>
     <ul class="post-list">
     {{range .}}
         <li class="post-item">
